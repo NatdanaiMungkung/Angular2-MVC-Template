4c8e92f3d15a1e1b229be3cba5bc854d2db0cb7b
Jaime Still <jpstill85@gmail.com>
2016-08-05 09:55:13 -0400

    Update typo in rxjs-operators

--- Angular2.MVC/app/rxjs-operators.ts
+++ Angular2.MVC/app/rxjs-operators.ts
@@ -1,4 +1,4 @@
-﻿import 'rxsj/add/observable/of';
+﻿import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
